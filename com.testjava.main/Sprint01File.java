79be22c54fce672a92ac7003e06e39108899f7f7
Siva <sivakrishnamits@gmail.com>
2021-07-10 22:26:17 +0530

    resolved the merge conflict

--- com.testjava.main/Sprint01File.java
+++ com.testjava.main/Sprint01File.java
@@ -2,7 +2,12 @@
 public class Sprint01File
 {
 	public static void main(String[] args) {
+		
 		System.out.println("sprint01 file");
+
 		System.out.println("added new line");
+
+		System.out.println("added for stashing");
+
 	}
 }
